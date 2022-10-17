bab2d4cbc389db4c547e53ec5e17405cc1471a1a
Carlos Fuerte <113551817+CursedCola@users.noreply.github.com>
2022-10-17 19:52:37 +0000

    Still trying on 4

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+from random import randint, choice
+
+
 # The task is this: the program will ask for the user's name and then it will say
 # something like: “Well John, I've thought of a number between 1 and 100 and you
 # have only eight tries to guess it. What number do you think it is?” On each try, the
 # # player will say a number and the program can answer for different things.
+print("Well John I've thought of a number between 1 and 100 and you have only eight tries to guess it. What number do you think it is?")
 
+secretNumber = randint(1, 100)
+
+guess = int(input("What your guess John? (NO DECIMALS): "))
+
+attempts = 0
 
 # 1 If the number the user said is less than 1 or greater than 100, it will tell them that  he/she has chosen a number that is out of play.
 
-
+if guess < 1 or guess > 100:
+  print("You have chosen a number that is out of play")
 
 # 2 If the number the user chose is less than the number the program thought of, it will tell them that the answer is wrong, and that he/she chose a lower number than the secret number.
+
+if guess < secretNumber:
+  print("Your answer is wrong, because it is lower than the secret number")
+  
 
 
 # 3 If the user chose a number greater than the secret number, it will let them know that it was greater.
 
+if guess > secretNumber:
+  print("Your answer is wrong, because it is greater than the secret number")
 
 # 4 And if the user got the secret number right, they will be informed that they have won,and how many tries that has taken them.
+
+if secretNumber == guess:
+  print("You have won")
+  attempts += 1
+if guess == number:
+  print(f"Correct! You used {attempts} attempts!")
 
 
 
