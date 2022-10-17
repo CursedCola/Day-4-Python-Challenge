8c7de0896a67ea87664e63236b086e3dd247ea05
Carlos Fuerte <113551817+CursedCola@users.noreply.github.com>
2022-10-17 19:59:56 +0000

    We've planned halfway and getting closer to making a program capable of playing the game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from random import randint, choice
 # # player will say a number and the program can answer for different things.
 print("Well John I've thought of a number between 1 and 100 and you have only eight tries to guess it. What number do you think it is?")
 
-secretNumber = randint(1, 100)
+secretNumber = randint(1, 5)
 
 guess = int(input("What your guess John? (NO DECIMALS): "))
 
@@ -15,28 +15,33 @@ attempts = 0
 
 # 1 If the number the user said is less than 1 or greater than 100, it will tell them that  he/she has chosen a number that is out of play.
 
-if guess < 1 or guess > 100:
+if guess < 1 or guess > 5:
   print("You have chosen a number that is out of play")
+elif guess < 1 or guess > 5:
+  input("Pick another number: ")
 
 # 2 If the number the user chose is less than the number the program thought of, it will tell them that the answer is wrong, and that he/she chose a lower number than the secret number.
 
 if guess < secretNumber:
   print("Your answer is wrong, because it is lower than the secret number")
-  
+elif guess < secretNumber:
+  input("Pick another number: ")
 
 
 # 3 If the user chose a number greater than the secret number, it will let them know that it was greater.
 
 if guess > secretNumber:
   print("Your answer is wrong, because it is greater than the secret number")
+elif guess > secretNumber:
+  input("Pick another number: ")
 
 # 4 And if the user got the secret number right, they will be informed that they have won,and how many tries that has taken them.
 
 if secretNumber == guess:
   print("You have won")
   attempts += 1
-if guess == number:
-  print(f"Correct! You used {attempts} attempts!")
+elif guess == secretNumber:
+  print(f"You have won! You used {attempts} attempts!")
 
 
 
